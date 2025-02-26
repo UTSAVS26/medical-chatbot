15293a620140e5e3117650f4a90477c210f4e568
UTSAVS26 <utsavsinghal26@gmail.com>
2025-02-26 10:54:23 +0530

    Folder Structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "Medical Chatbot",
+    version = "0.1",
+    author = "Utsav Singhal",
+    author_email = "utsavsinghal26@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+)
